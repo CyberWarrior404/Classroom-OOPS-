86c7ad474f6968cc12c4e06be553f2aaaecbcee5
CyberWarrior404 <mdmor@icloud.com>
2025-03-08 00:59:28 -0800

    Add files via upload

--- Classroom_OOPS.py
+++ Classroom_OOPS.py
@@ -1,19 +1,21 @@
-class classroom():
-    def __init__(self,not_verysmart,average,Smart,teacher):
-        self.not_verysmart=not_verysmart
-        self.average=average
-        self.Smart=Smart
-        self.teacher=teacher
+class Classroom():
+    def __init__(self,not_verysmart,average,Smart,teacher,principle):
+        self.ns=not_verysmart
+        self.avrg=average
+        self.Smrt=Smart
+        self.t=teacher
+        self.p=principle
+        print(not_verysmart)
     def definton_of_class(self):
         print("not_verysmart")
         print("average")
         print("Smart")
         print("teacher")
-
-object=classroom("D","B","A/A+","he/she does the grades")
+        print(self.ns)
+object=Classroom("D","B","A/A+","he/she does the grades","Mr.Bobbyilius")
 
 print(object.definton_of_class())
 print(object.not_verysmart)
 print(object.average)
 print(object.Smart)
-print(object.teacher)
+print(object.teacher) 
